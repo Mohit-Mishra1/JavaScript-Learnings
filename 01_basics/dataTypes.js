274baa38ca2be64607acb58fd7c25a56c7e5d093
Mohit-Mishra1 <128150107+Mohit-Mishra1@users.noreply.github.com>
2024-05-22 03:58:27 +0000

    data types summary

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+// let a = NaN;
+// console.log(typeof a)
+
+let name = "Mohit"
+
+let str = new String ("hello")
+
+
+console.log(typeof name);
+
+// console.log(obj.course);
+
+console.log(typeof str);
+
+console.log(name[1]);
+
+console.log(name.length)
+str = (str[0].toUpperCase())
+
+console.log(str);
+
+console.log(Math.round(4.49));
