0e160bf656a2087829139225c4d80d0607207644
Mohit-Mishra1 <128150107+Mohit-Mishra1@users.noreply.github.com>
2024-05-24 01:17:48 +0000

    map filter reduce

---
+++ 04_filter_map_reduce/02_map.js
@@ -0,0 +1,29 @@
+// const arr = [1,2,3,4,5]
+
+// const newArr = arr.map( (item) => item + 1 )
+
+// console.log(newArr);
+
+
+const obj = {
+    Mohit : 21,
+    asif : 26,
+    swati : 25
+}
+
+// let add = 0
+// let count = 0
+
+// for (const item in obj) {
+//     count++;
+//     // add += obj[item]
+//     console.log(obj[item]);
+// }
+
+
+// let avg = add / count
+
+
+// // console.log(`average of age is ${avg}`);
+
+console.log(Object.entries(obj));
